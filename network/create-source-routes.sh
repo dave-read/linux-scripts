3c360a7d2b59f79ca3d4006b450e686a0af240c2
azadmin <azadmin@vm1.hxnyzstekrrutimqjhuqicjowg.dx.internal.cloudapp.net>
2017-07-04 17:49:37 -0400

    fix typo

--- network/create-source-routes.sh
+++ network/create-source-routes.sh
@@ -38,14 +38,19 @@ NICS_PER_VM=4
 IP_PER_NIC=25
 
    for((nic=1; nic<NICS_PER_VM;++nic));do
-        routResult=$(ip route add defaullt via ${DEFAULT_ROUTE} dev eth${nic} table ${nic})
-        devResult=$(ip rule add iif eth${nic} table ${nic})
+        #routResult=$(ip route add defaullt via ${DEFAULT_ROUTE} dev eth${nic} table ${nic})
+        routeAddCmd="ip route add default via ${DEFAULT_ROUTE} dev eth${nic} table ${nic}"
+        echo $routeAddCmd
+        #devResult=$(ip rule add iif eth${nic} table ${nic})
+        devAddCmd="ip rule add iif eth${nic} table ${nic}"
+        echo $devAddCmd
         for((ipcfg=0; ipcfg<IP_PER_NIC;++ipcfg));do
             ((node_ip= ((nic) * IP_PER_NIC) + ipcfg + 1))
             IP_ADDR=${VM_IP_PREFIX}.$node_ip
-            ipResult=$(ip rule add from ${IP_ADDR} table ${nic})
+            ipAddCmd="ip rule add from ${IP_ADDR} table ${nic}"
+            echo $ipAddCmd
         done
    done
 
-ip route flush cache
-ip rule show
+#ip route flush cache
+#ip rule show
